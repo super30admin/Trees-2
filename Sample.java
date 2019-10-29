bf99701ce8f7206998cd0c265fb0b26732576121
keerthitejakonuri <keerthi49100@gmail.com>
2019-10-29 00:31:11 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,18 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(N)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Yes.
+// Any problem you faced while coding this :Nope.
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return sum(root,0);
+    }
+    public int sum(TreeNode root, int sum){
+        if(null == root){return 0;}	// base case
+        sum = ((sum*10) + root.val);
+        if(null == root.left && null == root.right){return sum;}
+        return sum(root.left,sum) + sum(root.right,sum);
+    }
+}
