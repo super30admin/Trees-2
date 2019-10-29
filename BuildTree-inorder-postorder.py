6f11b642d9c182c2f93dc784f0bf97a9cd082b79
Sudha S R <sudha.ramc@gmail.com>
2019-10-29 16:01:38 -0700

    O(n^2) solution

---
+++ BuildTree-inorder-postorder.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+#This is O(n^2) solution as we are searching inrder array for the root 
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if len(postorder) == 0:
+            return None
+        rootVal = postorder[-1]
+        rootidx = -1
+        for index, val in enumerate(inorder):
+            if val == rootVal:
+                rootidx = index
+        root = TreeNode(rootVal)
+        num_rightelements = len(inorder) - rootidx - 1
+        inleft = inorder[:rootidx]
+        postleft = postorder[:rootidx]
+        inright = inorder[rootidx + 1:]
+        postright = postorder[rootidx:len(postorder) - 1]
+        root.left = self.buildTree(inleft, postleft)
+        root.right = self.buildTree(inright, postright)
+        return root
