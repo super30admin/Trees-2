bfc96ddb5b2d7b4e8ece7c9a9b66abb9a0c44ace
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2020-04-09 12:13:04 -0500

    Create sumRootToLeafNumbers.java

---
+++ sumRootToLeafNumbers.java
@@ -0,0 +1,31 @@
+//time - O(n)
+//space - O(h) - height of tree
+//function calls
+        // (4, 0) -> (9, 40) -> (5, 490) -> adds 495 to sum
+        //                   -> (1, 490) -> adds 491 to sum
+        //        -> (0, 40) -> adds 40 to sum
+                        
+class Solution {
+    int sum = 0;
+    public int sumNumbers(TreeNode root) {
+        sumUtil(root, 0);
+        return sum;
+    }
+    
+    public void sumUtil(TreeNode root, int currentSum) {
+        //base
+        if(root == null)
+        {
+            return;
+        }
+        if(root.left == null && root.right == null)
+        {
+            sum += root.val + currentSum;
+            return;
+        }
+        //logic
+        currentSum = (currentSum + root.val) * 10;
+        sumUtil(root.left, currentSum);
+        sumUtil(root.right, currentSum);
+    }
+}
