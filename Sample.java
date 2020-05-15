cd5fb31ff686f328b60f4a933f01497ea0ce19f9
navnitss <43095390+navnitss@users.noreply.github.com>
2020-05-14 21:24:33 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -4,4 +4,29 @@
 // Any problem you faced while coding this :
 
 
-// Your code here along with comments explaining your approach
+------------------------------------------------------------------------------
+
+// Time Complexity :O(n)
+// Space Complexity :O(h)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+class Solution {
+  int rootToLeaf = 0;
+
+  public void preorder(TreeNode r, int currNumber) {
+    if (r != null) {
+      currNumber = currNumber * 10 + r.val;
+      // if it's a leaf, update root-to-leaf sum
+      if (r.left == null && r.right == null) {
+        rootToLeaf += currNumber;
+      }
+      preorder(r.left, currNumber);
+      preorder(r.right, currNumber) ;
+    }
+  }
+
+  public int sumNumbers(TreeNode root) {
+    preorder(root, 0);
+    return rootToLeaf;
+  }
+}
