01fd4d92a028d293bd9247cf0b20b0c0b698f955
navnitss <43095390+navnitss@users.noreply.github.com>
2020-05-15 22:03:15 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,8 +1,45 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
-
+//time complexity: O(n)
+//space complexity: O(h)
+//where n is the number of elements in tree. h is the height tree
+import java.util.*;
+class ConstructTreeSolution {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    int idx;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(postorder.length ==0) return null;
+        idx = postorder.length - 1;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++)
+        {
+            map.put(inorder[i],i);
+        }
+        return buildTree_dfs(0, postorder.length - 1, postorder, map);
+    }
+    
+    public TreeNode buildTree_dfs(int min, int max, int[] post, Map<Integer, Integer> map)
+    {
+        
+        if(min > max || idx < 0)
+            return null;
+        TreeNode root = null;
+        
+        int pos = map.get(post[idx]);
+        if(pos <= max && pos >= min)
+        {
+            root = new TreeNode(post[idx]);
+            idx--;
+            root.right= buildTree_dfs(pos+1, max, post, map);
+            root.left= buildTree_dfs(min, pos-1, post, map);
+         
+        }
+        return root;
+    }
+}
 
 ------------------------------------------------------------------------------
 
