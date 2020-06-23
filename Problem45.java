dd88ba54773c882ced9751606d60ba6d9dba94b2
RamaAncha <66054290+RamaAncha@users.noreply.github.com>
2020-06-22 23:15:05 -0400

    Update Problem45.java

--- Problem45.java
+++ Problem45.java
@@ -1,45 +1,25 @@
-Executed in leetcode
+Executed in leetcode - Yes
+Time Complexity-o(n)
+ space complexity - o(n) for recursive stack
 
-public class Solution{
- int calValue=0,totalSum=0;
-    void preOrderTraversalSum(Node node,int calValue){
+class Solution {
+    public int sumNumbers(TreeNode root) {
+             preOrderTraversalSum(root,calValue);
+        return totalSum;
+    }
+    
+     int calValue=0,totalSum=0;
+    void preOrderTraversalSum(TreeNode node,int calValue){
         if(node==null) return;
         
         //  process  Node
         //Identify leaf and get the sum value
         if(node.left==null && node.right==null){
-            totalSum+= calValue*10+node.value;
+            totalSum+= calValue*10+node.val;
         }else{
-            calValue=calValue*10+node.value;
+            calValue=calValue*10+node.val;
         }
         preOrderTraversalSum(node.left,calValue);
         preOrderTraversalSum(node.right,calValue);
     }
-    
-    int sumOfPaths(){
-        populateTree();
-        preOrderTraversalSum(root,calValue);
-        return totalSum;
-    }
-    
-     Node root;
-    void populateTree(){
-        root= new Node(4);
-        root.left= new Node(9);
-        root.left.left= new Node(5);
-        root.left.right= new Node(1);
-        
-        root.right= new Node(0);
-    }
-    
-      class Node{
-       public Node left;
-        public Node right;
-        int value;
-        
-        public Node(int val){
-            value=val;
-        }
-                
-    } 
-    }
+}
