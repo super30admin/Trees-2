252a2f6cbfde1eeecd9c6c7343f1cc4c8a4e27e6
Hanish Talasila <t.hanishsai@gmail.com>
2020-10-07 17:53:32 -0600

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,7 +1,51 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(N)+O(1)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this : No
 
 
-// Your code here along with comments explaining your approach
+// Your code here along with comments explaining your approach: Using 
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int post_index;
+    int[] inorder;
+    int[] postorder;
+    HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+       this.inorder = inorder;
+       this.postorder = postorder;
+        this.post_index = postorder.length-1;
+        int idx = 0;
+        for(Integer val: inorder){
+            hmap.put(val, idx++);
+        }
+        return bstNode(0, inorder.length-1);
+    }
+    
+    public TreeNode bstNode(int left, int right){
+        if(left > right)
+            return null;
+        int root_val = postorder[post_index--];
+        TreeNode root = new TreeNode(root_val);
+        
+        int idx = hmap.get(root_val);
+        root.right = bstNode(idx+1, right);
+        root.left = bstNode(left, idx-1);
+        return root;
+    }
+}
