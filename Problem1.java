f47560f861959dd7619783cbeb0fda27ff5d4d53
Hanish Talasila <t.hanishsai@gmail.com>
2020-10-07 17:55:31 -0600

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -3,8 +3,7 @@
 // Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this : No
 
-
-// Your code here along with comments explaining your approach: Using 
+// Your Approach: Using recurssion method to call the function at every node
 
 /**
  * Definition for a binary tree node.
