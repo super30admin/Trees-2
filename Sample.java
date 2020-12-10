a65236db0ea840e8ae7f4a7dff5ea6b8120ac44d
prabhatmishra <prabhatm@usc.edu>
2020-12-10 03:03:03 -0800

    commiting trees2 code

--- Sample.java
+++ Sample.java
@@ -1,7 +1,20 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Yes
+
+
+// 2 approaches store computed sum while doing inorder traversal
+// recursive add sum from left and right
+// recursice pproach 2 compute suma nd write code in inorder way
+
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Yes
 
 
 // Your code here along with comments explaining your approach
+// explore nodes in right and then in left from inorder
+// decremnt index of postorder to know the root at each level
+// partition inorder using value from postorder. 
