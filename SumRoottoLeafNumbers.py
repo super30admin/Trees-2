6070a913400bd70d2a3c83b548eef1c44b9a2259
pranati05 <pranati05@gmail.com>
2021-01-11 22:50:01 -0800

    Completed Trees-2

---
+++ SumRoottoLeafNumbers.py
@@ -0,0 +1,38 @@
+# Time Complexity :
+# Space Complexity :
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this :
+
+# Your code here along with comments explaining your approach
+# Using Iterative Preorder Traversal and storing node and sum for each node in the left and right subtree on stack
+# If the node is the leaf node that is it doesnt have left or right child then add the sum to res
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        def preorder(root):
+            stack = [(root, 0)]
+            res = 0
+            while stack:
+                node, num = stack.pop()
+                if node:
+                    num = num * 10 + node.val
+                    if not node.right and not node.left:
+                        res += num
+                    if node.right:
+                        stack.append((node.right, num))
+                    if node.left:
+                        stack.append((node.left, num))
+            return res
+
+        output = preorder(root)
+        return output
