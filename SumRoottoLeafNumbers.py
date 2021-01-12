90cf200006ebd55aa2a5c40e72d8d4b3251964bd
pranati05 <pranati05@gmail.com>
2021-01-11 22:50:38 -0800

    Completed Trees-2

--- SumRoottoLeafNumbers.py
+++ SumRoottoLeafNumbers.py
@@ -1,6 +1,6 @@
-# Time Complexity :
-# Space Complexity :
-# Did this code successfully run on Leetcode :
+# Time Complexity : O(H)
+# Space Complexity : O(H)
+# Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this :
 
 # Your code here along with comments explaining your approach
