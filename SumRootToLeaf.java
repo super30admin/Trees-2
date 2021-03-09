04786d1fe2b5e06dfd5c0a935470b5e7e6eb84cb
omkar <omkarvaidya13@gmail.com>
2021-03-09 18:53:45 -0500

    Added Comments to Trees-2

--- SumRootToLeaf.java
+++ SumRootToLeaf.java
@@ -20,6 +20,7 @@ class SumRootToLeaf {
 
     int result = 0;
     public int sumNumbers(TreeNode root) {
+        // Edge Case Checking
         if(root == null)
             return result;
         dfs(root, 0);
@@ -27,12 +28,16 @@ class SumRootToLeaf {
     }
     
     private void dfs(TreeNode root, int sum){
+        // Everytime we add the number to our sum till that point.
         sum = sum*10 + root.val;
+
+        // Make recursive calls to move further down in the tree
         if(root.left != null)
             dfs(root.left, sum);
         if(root.right != null)
             dfs(root.right, sum);
         
+        // When we reach the leaf node we add the sum value to our result and backtrack.
         if(root.left == null && root.right == null){
             result += sum;
             return;
