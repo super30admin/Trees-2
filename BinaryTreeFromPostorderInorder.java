b49feb474cd91af5a739d6b9be6bd3a9037414f1
omkar <omkarvaidya13@gmail.com>
2021-03-09 16:23:19 -0500

    Completed Trees-2

---
+++ BinaryTreeFromPostorderInorder.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BinaryTreeFromPostorderInorder {
+
+    // Time Complexity: O(n)    (where n -> no. of nodes in the tree)
+    // Space Complexity: O(n)
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int n = inorder.length;
+        if(n == 0)
+            return null;
+        return helper(inorder, 0, n, postorder, 0, n);
+    }
+    
+    private TreeNode helper(int[] inorder, int i1, int i2, int[] postorder, int p1, int p2){
+        if(i1 >= i2 || p1 >= p2)
+            return null;
+        
+        TreeNode root = new TreeNode(postorder[p2-1]);
+        int root_location = 0;
+        for(int i = i1; i < i2; i++){
+            if(postorder[p2-1] == inorder[i]){
+                root_location = i;
+                break;
+            }
+        }
+        
+        int left = root_location - i1;
+        root.left = helper(inorder, i1, i1+left, postorder, p1, p1+left);
+        root.right = helper(inorder, root_location+1, i2, postorder, p1+left, p2-1);
+        
+        return root;
+    }
+    
+}
---
+++ SumRootToLeaf.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class SumRootToLeaf {
+
+    // Time Complexity: O(n)    (where n -> no. of nodes in the tree)
+    // Space Complexity: O(h)   (where h -> height of the tree)
+
+    int result = 0;
+    public int sumNumbers(TreeNode root) {
+        if(root == null)
+            return result;
+        dfs(root, 0);
+        return result;
+    }
+    
+    private void dfs(TreeNode root, int sum){
+        sum = sum*10 + root.val;
+        if(root.left != null)
+            dfs(root.left, sum);
+        if(root.right != null)
+            dfs(root.right, sum);
+        
+        if(root.left == null && root.right == null){
+            result += sum;
+            return;
+        }
+    }
+}
