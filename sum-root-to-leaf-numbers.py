5fa26dd785ce283e7ea345423dc49f93f4a9032b
Suman Bharti <suman.bharti30@gmail.com>
2021-06-19 18:37:11 -0400

    solved

---
+++ sum-root-to-leaf-numbers.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        root_to_leaf = 0
+        stack = [(root, 0)]
+
+        while stack:
+            root, curr_number = stack.pop()
+            if root is not None:
+                curr_number = curr_number * 10 + root.val
+                # if it's a leaf, update root-to-leaf sum
+                if root.left is None and root.right is None:
+                    root_to_leaf += curr_number
+                else:
+                    stack.append((root.right, curr_number))
+                    stack.append((root.left, curr_number))
+
+        return root_to_leaf
