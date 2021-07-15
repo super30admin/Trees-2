aa02f99e2e1586b411a14cb4feb2934629ff8524
Aditya Dasani <dasani.ad@northeastern.edu>
2021-07-15 11:15:13 -0700

    Completed Trees-2

--- Sample.java
+++ Sample.java
@@ -1,7 +1,65 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(depth)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+class Solution {
+    HashMap<Integer, Integer> inorderMap;
+    int postIdx;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(inorder == null || postorder == null) {
+            return null;
+        }
+        postIdx = inorder.length-1;
+        
+        inorderMap = new HashMap<>();
+        
+        for(int i = 0; i < inorder.length; i++) {
+            inorderMap.put(inorder[i], i);
+        }
+        
+        return helper(inorder, postorder, 0 , inorder.length - 1);
+        
+    }
+    
+    private TreeNode helper(int[] inorder, int[] postorder, int start, int end) {
+        if(start > end) {
+            return null;
+        }
+        int rootVal = postorder[postIdx--];
+        TreeNode root = new TreeNode(rootVal);
+        
+        int rootIdx = inorderMap.get(rootVal);
+        
+        root.right = helper(inorder, postorder, rootIdx + 1, end);
+        root.left = helper(inorder, postorder, start, rootIdx-1);
+        return root;
+    }
+}
+
+// Time Complexity : O(depth)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
-// Your code here along with comments explaining your approach
+class Solution {
+    int result;
+    public int sumNumbers(TreeNode root) {
+        result = 0;
+        helper(root, 0);
+        return result;
+    }
+    
+    private void helper(TreeNode root, int tempSum) {
+        if(root == null) {
+            return;
+        }
+        tempSum = tempSum * 10 + root.val;
+        helper(root.left, tempSum);
+        if(root.left == null && root.right == null) {
+            result += tempSum;
+        }
+        helper(root.right,tempSum);
+    }
+}
