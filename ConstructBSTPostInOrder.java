c2676f44b686075254814f4cdf4e62ec46e01ca2
Tanreet Dhaliwal <tdhali@uwo.ca>
2021-08-11 16:04:39 -0700

    updated trees 2

--- ConstructBSTPostInOrder.java
+++ ConstructBSTPostInOrder.java
@@ -1,18 +1,40 @@
-class ConstructBSTPostInOrder {
+class Solution {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int index;
+
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        index = postorder.length - 1;
 
-        TreeNode root = new TreeNode(preorder[0]);
-
-        for (int i=1; i<preorder.length; i++) {
-
-            root.left = preorder[i * 2 + 1];
-            root.right = preorder[i * 2];
-
+        if (postorder.length == 0) {
+            return null;
         }
 
-        private void helper(inorder, postorder) {
-
+        for (int i=0; i<inorder.length; i++) {
+            map.put(inorder[i], i);
         }
+
+        return helper(postorder, 0, index);
+
+    }
+
+    private TreeNode helper(int[] postorder, int start, int end) {
+
+        if (start > end) {
+            return null;
+        }
+
+        int rootVal = postorder[index];
+        TreeNode root = new TreeNode(rootVal);
+        index--;
+        int rootIndex = map.get(rootVal);
+
+
+        root.right = helper(postorder, rootIndex + 1, end);
+        root.left = helper(postorder, start, rootIndex - 1);
+        //The right goes first since the new head that was obtained by decrementing postorder index is head of right tree since post order is left, right root
+
+        return root;
 
     }
 }
--- SumRootToLeaf.java
+++ SumRootToLeaf.java
@@ -1,31 +1,77 @@
-class SumRootToLeaf {
+class Solution {
+
+    public class Pair {
+
+        TreeNode node;
+        int value;
+
+        public Pair (TreeNode node, int value) {
+            this.node = node;
+            this.value = value;
+        }
+
+        public TreeNode getKey() {
+            return this.node;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+    }
     public int sumNumbers(TreeNode root) {
 
+        if (root == null) return 0;
+
+        Stack<Pair> stack = new Stack<>();
+        int currentSum = 0;
         int result = 0;
-        int currentSum= 0;
 
-        Stack<Pair<TreeNode, Integer>> stack = new Stack();
-        stack.push(new Pair(root, 0));
+        while (root != null || !stack.isEmpty()) {
 
-        while (!stack.isEmpty()) {
-            Pair<TreeNode, Integer> item = stack.pop();
-            currentSum = item.getValue();
-
-            if (root != null) {
+            while (root != null) {
                 currentSum = currentSum * 10 + root.val;
-
-                if (root.left == null && root.right == null) {
-                    result+=currentSum;
-                } else {
-                    stack.push(new Pair(root.right, currentSum));
-                    stack.push(new Pair(root.left, currentSum));
-                }
-
+                stack.push(new Pair (root, currentSum));
+                root = root.left;
             }
 
-        }
-        return result;
+            Pair p = stack.pop();
+            root = p.getKey();
+            currentSum = p.getValue();
+            if (root.right == null && root.left == null) {
+                result += currentSum;
+            }
+            root = root.right;
 
+        }
+
+        return result;
 
     }
 }
+
+class Solution {
+
+    int result;
+
+    public int sumNumbers(TreeNode root) {
+
+        if (root == null) return 0;
+        helper(root, 0);
+        return result;
+
+    }
+
+    private void helper(TreeNode root, int currSum) {
+
+        if (root == null) {
+            return;
+        }
+
+        currSum = currSum * 10 + root.val;
+        helper(root.left, currSum);
+
+        if (root.left == null && root.right == null) {
+            result += currSum;
+        }
+
+        helper(root.right, currSum);
