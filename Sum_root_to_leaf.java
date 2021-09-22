210e97472e81524a313acf64053737ac58ef4a76
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2021-09-22 18:25:44 -0400

    Create Sum_root_to_leaf.java

---
+++ Sum_root_to_leaf.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    class Pair{
+        TreeNode node;
+        int value;
+        
+        public Pair(TreeNode node, int value ){
+            this.node = node;
+            this.value = value;
+        }
+        
+        public TreeNode getKey(){
+            return this.node;
+        } 
+        
+        public int getValue(){
+            return this.value;
+        }
+    }
+    public int sumNumbers(TreeNode root) {
+        
+        Stack<Pair> st = new Stack<>();
+        int csum = 0;
+        int result = 0;
+        
+        while(root!= null || !st.isEmpty()){
+            while(root!=null){
+                
+                csum= csum*10 + root.val;
+                st.push(new Pair(root, csum));
+                root = root.left;
+            }
+            Pair p = st.pop();
+            root = p.getKey();
+            csum = p.getValue();
+            if(root.left == null && root.right == null){
+                result += csum;
+            }
+            
+            root = root.right;
+          
+            
+            
+        }
+        
+        return result;
+        
+    }
+}
+
+
+//TC O(N)
+//SC O(H)
