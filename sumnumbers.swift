8ab78295b8d17ee59ee9039769caf7bacd333954
panth1921 <panthdev96@gmail.com>
2021-10-19 01:40:22 -0400

    trees-2

---
+++ sumnumbers.swift
@@ -0,0 +1,28 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+    var sum = 0 
+    func sumNumbers(_ root: TreeNode?) -> Int {
+        if root == nil {
+            return sum
+        }
+        sumNumbersHelper(root,0)
+        return sum
+    }
+    func sumNumbersHelper(_ root: TreeNode?,_ current: Int) {
+        var current = current
+        guard let root = root else {
+            return  
+        } 
+        current = current*10 + root.val
+        if root.left == nil && root.right == nil {
+            sum += current
+        }
+        sumNumbersHelper(root.left,current)
+        sumNumbersHelper(root.right,current)
+    }
