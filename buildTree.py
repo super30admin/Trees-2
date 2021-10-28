4dfcd1476452275b24bbeff928dd1b867c27cd94
ag-tiwari <90532652+ag-tiwari@users.noreply.github.com>
2021-10-27 22:59:01 -0500

    Completed Trees-2

---
+++ buildTree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def buildTree(self, inorder, postorder):
+        if not inorder or not postorder:
+            return None
+        
+        root = TreeNode(postorder.pop())
+        inorderIndex = inorder.index(root.val)
+
+        root.right = self.buildTree(inorder[inorderIndex+1:], postorder)
+        root.left = self.buildTree(inorder[:inorderIndex], postorder)
+
+        return root
