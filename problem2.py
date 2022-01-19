37746168e41d4fd4c4ed48c414e6bcc23e080a88
nikhil <mandgenikhil@gmail.com>
2022-01-19 18:49:31 -0500

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,19 +1,18 @@
 #Time Complexity O(N)
-#Space Complexity O(N)
+#Space Complexity O(H)-height of tree
 class Solution:
-    def checktree(self,l,r):
-        if l == None and r ==None:
-            return True
-        if l==None and r !=None:
-            return False
-        if l!=None and r ==None:
-            return False
-        if l.val!=r.val:
-            return False
-        return self.checktree(l.left,r.right) and self.checktree(l.right,r.left)
-    
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return self.checktree(root.left,root.right)
+    def sumNumbers(self, root: TreeNode):
+        root_to_leaf = 0
+        st = [(root, 0) ]
         
-        
-        
+        while st:
+            root, curr_number = st.pop()
+            if root is not None:
+                curr_number = curr_number * 10 + root.val
+                if root.left is None and root.right is None:
+                    root_to_leaf += curr_number
+                else:
+                    st.append((root.right, curr_number))
+                    st.append((root.left, curr_number))
+                        
+        return root_to_leaf
