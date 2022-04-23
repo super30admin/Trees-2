11ffa71acc00255b31e119aea32f39022c49a768
tanmay24497 <tanmayagrawal244@gmail.com>
2022-04-22 17:09:57 -0700

    Iterative version added

--- Sum Root to Leaf Numbers.java
+++ Sum Root to Leaf Numbers.java
@@ -55,3 +55,27 @@ class Solution {
         helper(root.right,currSum*10+root.val);
     }
 }
+
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        if(root ==null) return 0;
+        Stack<TreeNode> s = new Stack<>();
+        Stack<Integer> nums = new Stack<>();
+        int sum = 0;
+        int num = 0;
+        while(root!=null || !s.isEmpty()){
+            while(root!=null){
+                s.push(root);
+                num = num*10 + root.val;
+                nums.push(num);
+                root = root.left;
+            }
+            root = s.pop();
+            num = nums.pop();
+            if(root.left==null&&root.right==null)
+                sum = sum+num;
+            root = root.right;
+        }
+        return sum;
+    }
+}
