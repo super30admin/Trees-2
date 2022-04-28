206741aaf23cc1c1be5801503cb4b034e2074559
Rujal <rujal8452@gmail.com>
2022-04-28 01:46:53 -0400

    Problem2

--- Problem2.java
+++ Problem2.java
@@ -1,30 +1,36 @@
-public class Problem2 {
 
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     * int val;
-     * TreeNode left;
-     * TreeNode right;
-     * TreeNode() {}
-     * TreeNode(int val) { this.val = val; }
-     * TreeNode(int val, TreeNode left, TreeNode right) {
-     * this.val = val;
-     * this.left = left;
-     * this.right = right;
-     * }
-     * }
-     */
+class Problem2 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 //    TC : O(n)
 //    Sc : O(h) where h = height of tree
 
     class Solution {
         int tSum;
+
         public int sumNumbers(TreeNode root) {
             if (root == null) return 0;
             rootSum(root, 0);
             return tSum;
         }
+
         private void rootSum(TreeNode root, int currSum) {
             if (root == null) return;
             if (root.left == null && root.right == null) {
@@ -35,3 +41,4 @@ public class Problem2 {
         }
     }
 }
+
