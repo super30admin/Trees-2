0d6680470e5956a30bc0da01b9c245bb01a80586
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-05-19 22:06:13 -0500

    Create BStTraversal.java

---
+++ BStTraversal.java
@@ -0,0 +1,22 @@
+
+//O(n) space|| O(n) - time
+
+// recursive solution
+class Solution {
+    public boolean validate(TreeNode root, Integer low, Integer high) {
+        // Empty trees are valid BSTs.
+        if (root == null) {
+            return true;
+        }
+        // The current node's value must be between low and high.
+        if ((low != null && root.val <= low) || (high != null && root.val >= high)) {
+            return false;
+        }
+        // The left and right subtree must also be valid.
+        return validate(root.right, root.val, high) && validate(root.left, low, root.val);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, null, null);
+    }
+}
