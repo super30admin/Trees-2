8426f29dd665daf7da14f7cb89938ca1f0365ddd
jayachandravvs <42566670+jayachandravvs@users.noreply.github.com>
2022-05-22 11:18:53 -0500

    Create SumRootToLeaf.java

---
+++ SumRootToLeaf.java
@@ -0,0 +1,21 @@
+// O(n) time || O(1) space 
+class Solution {
+  int rootToLeaf = 0;
+
+  public void preorder(TreeNode r, int currNumber) {
+    if (r != null) {
+      currNumber = currNumber * 10 + r.val;
+      // if it's a leaf, update root-to-leaf sum
+      if (r.left == null && r.right == null) {
+        rootToLeaf += currNumber;
+      }
+      preorder(r.left, currNumber);
+      preorder(r.right, currNumber) ;
+    }
+  }
+
+  public int sumNumbers(TreeNode root) {
+    preorder(root, 0);
+    return rootToLeaf;
+  }
+}
