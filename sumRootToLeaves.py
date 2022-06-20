10e502bf48405ee5f15efffe9c77366e166bfd14
poojitha bijjam <poojitha1589@gmail.com>
2022-06-20 15:42:03 -0500

    Update sumRootToLeaves.py

--- sumRootToLeaves.py
+++ sumRootToLeaves.py
@@ -29,3 +29,29 @@ class Solution:
             root = root.right
             
         return res
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    result = 0
+    
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        self.helper(root, 0)
+        return self.result
+    
+    def helper(self, root, currNum):
+        if root is None:
+            return
+        #logic
+        self.helper(root.left, currNum * 10 + root.val)
+        if root.left is None and root.right is None:
+            self.result += currNum * 10 + root.val 
+        self.helper(root.right, currNum * 10 + root.val)
+        
+        
+
+'''
