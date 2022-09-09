02fdd727c9a8aa3a3a96558a642a7a6d07ab3cfb
Rajendra Prasad Patil <111140820+rajendra7406-2@users.noreply.github.com>
2022-09-09 13:14:15 -0400

    added another approach

--- problem2.py
+++ problem2.py
@@ -18,23 +18,40 @@ DIFFICULTIES: I tried to solve, I couldn't. After watching the class, I was able
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        
-        global res
-        res = 0
-        def traverse(root, cs):
+    
+        def approach1(root):
             global res
-            if root.left == None and root.right == None: 
+            res = 0
+            def traverse(root, cs):
+                global res
+                if root.left == None and root.right == None: 
+                    cs = cs * 10 + root.val
+                    res += cs
+                    return
                 cs = cs * 10 + root.val
-                res += cs
+                if root.left != None:
+                    traverse(root.left, cs)
+                if root.right != None:
+                    traverse(root.right, cs)
+
                 return
-            cs = cs * 10 + root.val
+
+            traverse(root, 0)
+
+            return res
+        
+        def approach2(root, res):
+            if root.left == None and root.right== None:
+                return res * 10 + root.val
+            lv = 0
             if root.left != None:
-                traverse(root.left, cs)
+                lv = approach2(root.left, res * 10 + root.val) 
+            rv = 0
             if root.right != None:
-                traverse(root.right, cs)
-            
-            return
+                rv = approach2(root.right, res * 10 + root.val)
+            return lv + rv
         
-        traverse(root, 0)
         
-        return res
+        # return approach1(root)
+        return approach2(root, 0)
+    
