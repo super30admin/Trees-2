16819fd3b5d16810e5a8807afdb44a091ebc3947
meghadusane97 <dusanemegha97@gmail.com>
2022-09-26 11:28:27 -0700

    Solution to Trees-2

--- BinaryTreeFromInorderAndPostorder.java
+++ BinaryTreeFromInorderAndPostorder.java
@@ -1,7 +1,3 @@
-# Trees-2
-
-## Problem1 (https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/)
-```Java
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         return build(0,inorder.length-1,0,postorder.length-1,inorder,postorder);
@@ -41,58 +37,3 @@ class Solution {
         return 0;
     }
 }
-```
-
-## Problem2: sum-root-to-leaf-numbers
-
-```Java
-// Time Complexity : O(n)
-// Space Complexity : O(n)
-// Did this code successfully run on Leetcode : yes
-// Any problem you faced while coding this : no
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int sumNumbers(TreeNode root) {
-        Stack<TreeNode> st = new Stack<>();
-        Stack<Integer> numSt = new Stack<>();
-        
-        int sum = 0;
-        int currNum = 0;
-        
-        while(root != null || !st.isEmpty()){
-            while(root != null){
-                currNum = currNum * 10 + root.val;
-                st.push(root);
-                numSt.push(currNum);
-                root = root.left;
-            }
-            //pop
-            root = st.pop();
-            currNum = numSt.pop(); 
-            
-            //check leaf
-            if(root.left == null && root.right == null)
-                sum += currNum;
-            
-            root = root.right;
-        }
-        return sum;
-    }
-}
-```
-   
