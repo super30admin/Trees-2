6a54302c07ba7290a501ef7d6b4a63575d8a13d8
yashikanihalani <yashikanihalani@gmail.com>
2022-11-28 19:21:59 -0500

    updated buildtree

--- buildTree.java
+++ buildTree.java
@@ -1,30 +1,46 @@
-// Time Complexity : O(n)
-// Space Complexity :O(n) 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     HashMap<Integer,Integer> hm;
     int idx;
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(inorder==null || preorder==null || inorder.length==0) return null;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(inorder==null || postorder==null || inorder.length==0){
+            return null;
+        }
         hm=new HashMap<>();
+        int rootIdx=-1;
+        idx=postorder.length-1;
         for(int i=0;i<inorder.length;i++){
             hm.put(inorder[i],i);
         }
         
-        return recurse(preorder,inorder,0,inorder.length-1);
+        return recurse(postorder,inorder,0, postorder.length-1);
     }
     
-    private TreeNode recurse(int[] preorder, int[] inorder, int start, int end){
-        //base case
+    private TreeNode recurse(int[] postorder, int[] inorder,int start,int end){
         if(start>end){
             return null;
         }
+        
         //logic
-        int rootVal=preorder[idx++];
+        int rootVal=postorder[idx--];
         TreeNode root=new TreeNode(rootVal);
         int rootIdx=hm.get(rootVal);
-        root.left=recurse(preorder,inorder,start,rootIdx-1);
-        root.right=recurse(preorder,inorder,rootIdx+1,end);
+        root.right=recurse(postorder,inorder,rootIdx+1,end);
+        root.left=recurse(postorder,inorder,start,rootIdx-1);
         return root;
-        
     }
 }
