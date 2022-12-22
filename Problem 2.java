7a8edfbf26db44ed2e653777633b31b54fdf863c
Syed Ammad Hassan <ammad13k@gmail.com>
2022-12-22 17:59:30 -0600

    Update Problem 2.java

--- Problem 2.java
+++ Problem 2.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(n)
-// Space Complexity : O(1)
+// Space Complexity : O(h) ,where h is the height of the tree
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : leaf node sum created problems
 
