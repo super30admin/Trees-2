80e4c023a62e03b9a4d62e212a2ba57eff9b9618
NAVEEN US <88378808+usnaveen@users.noreply.github.com>
2023-02-23 18:33:43 +0530

    Create tree_inorder_postoder.py

---
+++ tree_inorder_postoder.py
@@ -0,0 +1,20 @@
+# Author: Naveen US
+# Titile: Construct Binary Tree from Inorder and Postorder Traversal
+
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+# Did the code run on leetcode: Yes
+      
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        map_inorder = {}
+        for i, val in enumerate(inorder): map_inorder[val] = i
+        def recur(low, high):
+            if low > high: return None
+            x = TreeNode(postorder.pop())
+            mid = map_inorder[x.val]
+            x.right = recur(mid+1, high)
+            x.left = recur(low, mid-1)
+            return x
+        return recur(0, len(inorder)-1)
