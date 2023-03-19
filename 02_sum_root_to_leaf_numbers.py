fbe6bd15389660dd9f26121dea7cfe4f6020ccc0
Mitalee Minde <mitaleeminde@gmail.com>
2023-03-18 21:25:37 -0400

    Completed the problems

---
+++ 02_sum_root_to_leaf_numbers.py
@@ -0,0 +1,19 @@
+'''
+TC: O(n) to traverse all nodes
+SC: O(n) for recursive stack if all the nodes are present on the left of parent node or O(h) he is the height of binary tree
+
+'''
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        sumTotal = 0
+        def recursion(root, sumTotal):
+            if not root:
+                return 0
+            sumTotal = sumTotal*10 + root.val
+            if (not (root.left or root.right)): # determine if this is the last node
+                return sumTotal
+            return recursion(root.left, sumTotal) + recursion(root.right, sumTotal)
+        
+        return recursion(root, sumTotal)
+        
--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 ## Problem1 (https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/)
 ## Problem2 (https://leetcode.com/problems/sum-root-to-leaf-numbers/)
-   
