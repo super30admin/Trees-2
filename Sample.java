525542fd7f57d9e701b5135f6f17e4b92bc6ad85
Apeksha Kotian <apekshakotian@Apekshas-MacBook-Pro.local>
2023-10-18 17:11:37 -0500

    Completed Trees-2 Problem 2

--- Sample.java
+++ Sample.java
@@ -1,7 +1,41 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n)
+// Space Complexity : O(h)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : I didnt face any problem while coding this
 
 
 // Your code here along with comments explaining your approach
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result;
+    public int sumNumbers(TreeNode root) {
+        helper(root, 0);
+        return result;
+    }
+
+    private void helper(TreeNode root, int currNum){
+        if(root == null) return;
+        
+        currNum = currNum * 10 + root.val;
+        if(root.left == null && root.right == null){
+            result += currNum;
+        }
+        helper(root.left, currNum);
+        helper(root.right, currNum);
+    }
+}
